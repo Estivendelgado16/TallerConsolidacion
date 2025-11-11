e6219a1a49543674bb10e7e044e13a994be96707
Estivendelgado16 <brallamdelgado351@gmail.com>
2025-11-11 13:08:01 -0500

    agg func calc

--- nivel2/calculadora.py
+++ nivel2/calculadora.py
@@ -2,18 +2,33 @@ def sumar():
     num1 = float(input("Ingresa el primer numero: "))
     num2 = float(input("Ingresa el segundo numero: "))
 
+    resultado = num1 + num2
+    print(f"El resultado es: {resultado}")
+
 def restar():
     num1 = float(input("Ingresa el primer numero: "))
     num2 = float(input("Ingresa el segundo numero: "))
+
+    resultado = num1 - num2
+    print(f"El resultado es: {resultado}")
+
 
 def multiplicar():
     num1 = float(input("Ingresa el primer numero: "))
     num2 = float(input("Ingresa el segundo numero: "))
 
-def division():
-    num1 = float(input("Ingresa el primer numero: "))
-    num2 = float(input("Ingresa el segundo numero: "))
+    resultado = num1 * num2
+    print(f"El resultado es: {resultado}")
 
+def division():
+    num1 = float(input("Ingresa el primer número: "))
+    num2 = float(input("Ingresa el segundo número: "))
+
+    if num2 == 0:
+        print("No se puede dividir por 0")
+    else:
+        resultado = num1 / num2
+        print(f"El resultado es: {resultado}")
 
 
 print("CALCULADORA\n 1. suma\n 2. resta\n 3.multiplicacion\n 4.division\n 5. salir")
@@ -21,7 +36,7 @@ print("CALCULADORA\n 1. suma\n 2. resta\n 3.multiplicacion\n 4.division\n 5. sal
 while True:
 
     try:
-        opcion = int(input("Ingresa una opcion"))
+        opcion = int(input("Ingresa una opcion: "))
     except ValueError:
         print("Ingrese una opcion valida")
         continue
@@ -33,7 +48,7 @@ while True:
     elif opcion == 3:
         multiplicar()
     elif opcion == 4:
-        division
+        division()
     elif opcion == 5:
         print("Saliendo")
         exit()
